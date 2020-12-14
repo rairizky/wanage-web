3b1b38261b431a56118b3672ce43d92089ea58e8
Rafli Rai Rizky <rairizky37@gmail.com>
2020-12-14 10:10:50 +0700

    add form profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,5 +4,34 @@ class ProfileController < ApplicationController
   helper_method :current_user
 
   def index
+    @user = User.find(session[:user_id])
+    @get_user = @user.profile
+    @profile = Profile.new
+  end
+
+  def create
+    @user = User.find(session[:user_id])
+    @profile = @user.create_profile(profile_params)
+    if @profile.valid?
+      flash[:success] = 'Create profile success!'
+      redirect_to profile_index_path
+    else
+      
+    end
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+    @get_user = @user.profile
+    if @get_user.update(profile_params)
+      flash[:success] = 'Update profile success'
+    else
+
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:avatar, :dob, :phone, :address)
   end
 end
